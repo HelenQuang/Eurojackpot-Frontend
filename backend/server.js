cab07db22543d4434024045eb84d83d5f454937d
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-08-18 21:35:52 +0300

    Custom error handler

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,22 @@ import colors from "colors";
 
 import connectDB from "./config/db.js";
 import users from "./data/users.js";
+import userRoutes from "./routes/userRoutes.js";
+import lotteryRoutes from "./routes/lotteryRoutes.js";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+//User Routes:
+app.use("/api/users", userRoutes);
+
+//Lottery Routes:
+app.use("/api/lottery", lotteryRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -18,6 +28,10 @@ app.get("/", (req, res) => {
 app.get("/api/users", (req, res) => {
   res.json(users);
 });
+
+//Error Handler Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
