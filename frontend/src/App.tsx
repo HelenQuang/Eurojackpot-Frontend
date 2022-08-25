6d240b4ce3acea582a8d458e8b3deef4e0d2104d
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-08-25 20:43:48 +0300

    Transaction page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import axios from "axios";
 
 import Container from "./components/layout/Container";
 import NavBar from "./components/layout/NavBar";
@@ -17,24 +19,50 @@ import GameHistoryPage from "./pages/GameHistoryPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
+  const [clientID, setClientID] = useState(
+    "AWTmOi9rhej7hF49jA6GsNCtKWIXkDRZSDabgew-hqmS_wNDFyu8Qy33xW8v7mTO1VWRUTewvS0I_3Jg"
+  );
+
+  // useEffect(() => {
+  //   const getClientId = async () => {
+  //     const { data: clientId } = await axios.get("/api/config/paypal");
+
+  //     setClientID(clientId);
+  //   };
+
+  //   if (!window.paypal) {
+  //     getClientId();
+  //   }
+  // }, []);
+
+  const initialOptions = {
+    "client-id": clientID,
+    currency: "EUR",
+    intent: "capture",
+  };
+
   return (
     <>
-      <NavBar />
-      <Hero />
-      <Container>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/rule" element={<RulePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/transaction" element={<TransactionPage />} />
-          <Route path="/history" element={<GameHistoryPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Container>
-      <Footer />
+      {clientID && (
+        <PayPalScriptProvider options={initialOptions}>
+          <NavBar />
+          <Hero />
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/rule" element={<RulePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/transaction" element={<TransactionPage />} />
+              <Route path="/history" element={<GameHistoryPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Container>
+          <Footer />
+        </PayPalScriptProvider>
+      )}
     </>
   );
 };
--- frontend/src/pages/TransactionPage.tsx
+++ frontend/src/pages/TransactionPage.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  CircularProgress,
+  Alert,
+} from "@mui/material";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+
+const topupAmount = [10, 20, 30, 50];
 
 const TransactionPage = () => {
-  return <div>TransactionPage</div>;
+  const [{ isPending, isResolved, isRejected }] = usePayPalScriptReducer();
+
+  // const createOrder = (data, actions) => {
+  //   return actions.order.create({
+  //     purchase_units: [
+  //       {
+  //         amount: { value: order.totalPrice },
+  //       },
+  //     ],
+  //   });
+  // };
+
+  // const successPaymentHandler = (data, actions) => {
+  //   return actions.order.capture().then((details) => {
+  //     dispatch(payOrder(id, details));
+  //   });
+  // };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <h2>Topup Money</h2>
+      <FormControl>
+        <FormLabel id="top-up" style={{ marginBottom: "1rem" }}>
+          Please select the amount of money you want to topup
+        </FormLabel>
+        <RadioGroup
+          aria-labelledby="top-up"
+          defaultValue="10"
+          style={{ alignItems: "center" }}
+        >
+          {topupAmount.map((amount) => (
+            <FormControlLabel
+              key={amount}
+              value={amount}
+              control={<Radio />}
+              label={`${amount} Euros`}
+            />
+          ))}
+        </RadioGroup>
+      </FormControl>
+
+      {isPending && <CircularProgress />}
+      {isRejected && <Alert severity="error">SDK loading error</Alert>}
+      {isResolved && (
+        <PayPalButtons
+        // createOrder={createOrder}
+        // onApprove={successPaymentHandler}
+        // className="paypal-btn"
+        />
+      )}
+    </div>
+  );
 };
 
 export default TransactionPage;
