733f756ad3c4b3862bf288bb0c975d6904d38b62
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-08-30 18:15:00 +0300

    login and register user in BE

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -54,6 +54,7 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//Encrypt the password
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
